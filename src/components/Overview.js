507098d58513eea5d658b0f494110e566d6eecff
stanochka <krozzochka@gmail.com>
2021-08-04 17:16:52 +0300

    Overview class to function

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,26 +1,20 @@
 import React from "react";
 
-class Overview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Overview = (props) => {
+  const { tasks, handleDelete } = props;
 
-  render() {
-    const { tasks, handleDelete } = this.props;
-
-    return (
-      <ul>
-        {tasks.map((task, i) => {
-          return (<li key={task.id}>
-                    <strong>{i+1})</strong>
-                    {task.text}
-                    <button id={task.id} onClick={handleDelete}>Delete</button>
-                  </li>
-                  );
-        })}
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      {tasks.map((task, i) => {
+        return (<li key={task.id}>
+                  <strong>{i+1})</strong>
+                  {task.text}
+                  <button id={task.id} onClick={handleDelete}>Delete</button>
+                </li>
+                );
+      })}
+    </ul>
+  );
 }
 
 export default Overview;
